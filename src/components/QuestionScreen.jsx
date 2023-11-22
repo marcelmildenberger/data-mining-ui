d841563d4f4a3182417b91dff90eb8aef81327ce
Marcel Mildenberger <marcel.mildenberger@sap.com>
2023-11-22 16:55:01 +0100

    improvements

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Question from './Question';
 import Result from './Result';
 
+const mockResult = [{id:1, partyName: "FDP", percentage: 0.2},{id:2, partyName: "SPD", percentage: 0.3}, {id:3, partyName: "Bündnis90", percentage: 0.5}, , {id:4, partyName: "CDU&CSU", percentage: 0.1}]
 const mockData = [{id: 1, question: "Yes or No?"}, {id: 2, question: "Yes or Yes?"}, {id: 3, question: "No or No?"}, {id: 1, question: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."}]
 
 function QuestionScreen() {
@@ -14,17 +15,22 @@ function QuestionScreen() {
 
 
   useEffect(() => {
-    //fetch questions on render
-    setQuestions(mockData)
+    fetchQuestions();
   },[])
 
   // eslint-disable-next-line
-  const fetchQuestions = async () => {return null;}
+  const fetchQuestions = async () => {
+    setQuestions(mockData)
+    return;
+  }
 
   // eslint-disable-next-line
-  const fetchResult = async () => {
-    setResult(null)
-    return null;}
+  const postResult = async () => {
+    const sortedAnswers = answers.sort((a,b) => a.id - b.id)
+    const sortedResult = mockResult.sort((a,b) => b.percentage - a.percentage)
+    setResult(sortedResult)
+    return;
+  }
 
 
 
@@ -33,9 +39,10 @@ function QuestionScreen() {
       {!started ? <div onClick={() => setStarted(true)} className='py-2 text-center w-60 bg-[#f75204] text-white rounded-xl text-lg cursor-pointer ease-in-out duration-150 hover:bg-[#fda880]'>
         Get started
     </div> : <div className='flex flex-col gap-10 items-center'>
-      {questions.map((item,index) => index < currentQuestion ? <Question key={index} questionsLength={questions.length} index={index} question={item} setCurrentQuestion={setCurrentQuestion} setAnswer={(answer) => setAnswers((prev) => [...prev, {id: item.id, question: item.question, answer:answer}])} /> : null)}
-      {questions.length === currentQuestion - 1 ? <div onClick={() => fetchResult()} className='py-2 text-center w-60 sm:w-80 bg-[#f75204] text-white rounded-xl text-lg cursor-pointer ease-in-out duration-150 hover:bg-[#fda880]'>Get Result!</div> : null}
+      {questions.map((item,index) => index < currentQuestion ? <Question key={index} questionsLength={questions.length} index={index} question={item} setCurrentQuestion={setCurrentQuestion} setAnswer={(answer) => setAnswers((prev) => prev?.filter((val) => val?.id === item.id).length === 0 ? [...prev, {id: item.id, question: item.question, answer:answer}] : [...prev.filter(val => val?.id !== item.id), {id: item.id, question: item.question, answer:answer} ])} /> : null)}
+      {questions.length === currentQuestion - 1 ? <div onClick={() => postResult()} className='py-2 text-center w-60 sm:w-80 bg-[#f75204] text-white rounded-xl text-lg cursor-pointer ease-in-out duration-150 hover:bg-[#fda880]'>{!result ? "Get result!" : "Update result!"}</div> : null}
     </div> }
+    {result ? <Result result={result} /> : null}
     </div>
   )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Result() {
+function Result(props) {
+  const {result} = props;
   return (
-    <div>Result</div>
+    <div className='mt-10 flex flex-col items-center'>
+      <div className='font-bold text-4xl border-b-2 w-40 text-center mb-8'>Result</div>
+      <div className='flex flex-col gap-2'>
+      {result.map((item, index) => <div className={`text-center text-[${30-index*10}px] opacity-[${1-(index/100)*20}]`} key={index}>{item.percentage*100}% {item.partyName}</div>)}
+      </div>
+    </div>
   )
 }
 
